cdbdc21b2f096e912540a1615200d72ec8aecf67
linakozyrieva <eaa24liko@students.eaaa.dk>
2025-03-05 12:09:16 +0100

    components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,11 @@
-import React from "react";
 
-function Footer() {
-  return <div>NEW FOOTER</div>;
+import styles from "./Footer.module.css"; // Import CSS module
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>© {new Date().getFullYear()} New Horizon Travel. All rights reserved.</p>
+      <p>Contact: contact@newhorizontravel.com | +1 (555) 123-4567</p>
+    </footer>
+  );
 }
-
-export default Footer;
